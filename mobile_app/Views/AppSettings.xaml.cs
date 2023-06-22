6b2a8ef174a279511873abb16c1115b1e32d80d8
dakota kyle <dakotamkyle@gmail.com>
2023-06-22 15:41:52 -0400

    Created event handlers for AppSettings

--- mobile_app/Views/AppSettings.xaml.cs
+++ mobile_app/Views/AppSettings.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using mobile_app.Services;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace mobile_app.Views
 {
@@ -15,6 +12,34 @@ namespace mobile_app.Views
         public AppSettings()
         {
             InitializeComponent();
+        }
+
+        private void ClearPrefrences_Clicked(object sender, EventArgs e)
+        {
+            Preferences.Clear();
+        }
+
+        async void LoadSampleData_Clicked(object sender, EventArgs e)
+        {
+            if (Settings.FirstRun)
+            {
+                await DatabaseService.LoadSampleData();
+                
+                Settings.FirstRun = false;
+
+                await Navigation.PopToRootAsync();
+
+            }
+        }
+
+        private async void ClearSampleData_Clicked(object sender, EventArgs e)
+        {
+            await DatabaseService.ClearSampleData();
+        }
+
+        private void Notifications_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
