06d11cd824c5dfafc9b3d7c2e5622ef1687cb44d
dakota kyle <dakotamkyle@gmail.com>
2023-06-26 12:54:45 -0400

    Added save, edit, and delete methods for GadgetEdit.

--- mobile_app/Views/GadgetEdit.xaml.cs
+++ mobile_app/Views/GadgetEdit.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using mobile_app.Models;
+using mobile_app.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,75 @@ namespace mobile_app.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GadgetEdit : ContentPage
     {
+
+        private readonly int _selectedGadgetId;
+
         public GadgetEdit()
         {
             InitializeComponent();
+        }
+        public GadgetEdit(Gadget gadget)
+        {
+            InitializeComponent();
+
+            _selectedGadgetId = gadget.Id;
+            GadgetName.Text = gadget.Name;
+            GadgetColorPicker.SelectedItem = gadget.Color;
+            CreationDatePicker.Date = gadget.CreationDate;
+        }
+
+        async void SaveGadget_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(GadgetName.Text))
+            {
+                await DisplayAlert("Missing name", "Please enter a name", "OK");
+            }
+
+            if (string.IsNullOrWhiteSpace(GadgetColorPicker.SelectedItem.ToString()))
+            {
+                await DisplayAlert("Missing color", "Please enter a color", "OK");
+            }
+
+            await DatabaseService.UpdateGadget(int.Parse(GadgetId.Text), GadgetName.Text, GadgetColorPicker.SelectedItem.ToString(), DateTime.Parse(CreationDatePicker.Date.ToString()));
+            await Navigation.PopAsync();
+        }
+
+        async void CancelGadget_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        async void DeleteGadget_Clicked(object sender, EventArgs e)
+        {
+            var answer = await DisplayAlert("Delete Gadget and related Widgets?", "Delete this Gadget?", "Yes", "No");
+
+            if (answer == true)
+            {
+                var id = int.Parse(GadgetId.Text);
+                
+                await DatabaseService.RemoveGadget(id);
+
+                await DisplayAlert("Gadget deleted.", "Gadget deleted", "OK");
+            }
+
+            await Navigation.PopAsync();
+        }
+
+        async void AddWidget_Clicked(object sender, EventArgs e)
+        {
+            var gadgetId = int.Parse(GadgetId.Text);
+
+            await Navigation.PushAsync(new WidgetAdd(gadgetId));
+        }
+
+        async void WidgetCollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var widget = (Widget)e.CurrentSelection.FirstOrDefault();
+
+            if (e.CurrentSelection != null)
+            {
+                await Navigation.PushAsync(new WidgetEdit(Widget));
+            }
         }
     }
 }
